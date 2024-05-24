fa3485100ebb0dbcc9f4ad719c2a928992fea6b7
yuthzi <yuth89@163.com>
2024-05-24 18:19:34 +0800

    多值文本输入组件

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import MutilTextInput from './components/MutilTextInput/src/MutilTextInput.vue'
+</script>
+
+<template>
+  <main>
+    <div class="wrapper">
+      <MutilTextInput
+            name="item-input"
+            :values="['a', 'b', 'c']"
+            :label="`尺寸`"
+            :placeholder="`输入任意字符，每个元素之间用回车分割`"
+          />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+@media (min-width: 1024px) {
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
